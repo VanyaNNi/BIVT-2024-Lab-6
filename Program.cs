65e03257a4acbe3ddf4324a3346d9b1e7475ede8
VanyaNNi <93041998+VanyaNNi@users.noreply.github.com>
2025-03-07 23:37:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //#1
+
+            //White_1.Participant participant1 = new White_1.Participant("Лисицына", "Рубин");
+            //participant1.Jump(7.39);
+            //participant1.Jump(6.96);
+
+            //White_1.Participant participant2 = new White_1.Participant("Ушкаовка", "Василек");
+            //participant2.Jump(7.43);
+            //participant2.Jump(5.55);
+
+            //White_1.Participant[] participants = new White_1.Participant[2];
+            //participants[0] = participant2;
+            //participants[1] = participant1;
+
+            //foreach(var participant in participants)
+            //{
+            //    participant.Print();
+            //}
+
+            //White_1.Participant.Sort(participants);
+
+            //Console.WriteLine("После сортировки: ");
+            //Console.WriteLine();
+
+            //foreach (var participant in participants)
+            //{
+            //    participant.Print();
+            //}
+
+            //#2
+            //White_2.Participant participant1 = new White_2.Participant("Дмитрий","Лисицын");
+            //participant1.Jump(3.16);
+            //participant1.Jump(2.42);
+            //White_2.Participant participant2 = new White_2.Participant("Савелий", "Зайцев");
+            //participant2.Jump(1.28);
+            //participant2.Jump(1.97);
+            //White_2.Participant[] participants = new White_2.Participant[] {participant2, participant1};
+            //foreach(var  participant in participants)
+            //{
+            //    participant.Print();
+            //}
+            //White_2.Participant.Sort(participants);
+            //Console.WriteLine("После");
+            //foreach (var participant in participants)
+            //{
+            //    participant.Print();
+            //}
+
+            //#3
+
+            //White_3.Student student1 = new White_3.Student("Савелий","Полевой");
+            //student1.Lesson(3);
+            //student1.Lesson(4);
+            //student1.Lesson(4);
+            //student1.Lesson(3);
+            //student1.Lesson(3);
+            //student1.Lesson(5);
+            //student1.Lesson(5);
+            //student1.Lesson(4);
+            //student1.Lesson(4);
+            //student1.Lesson(0);
+
+            //White_3.Student student2 = new White_3.Student("Дарина","Козлова");
+            //student2.Lesson(4);
+            //student2.Lesson(5);
+            //student2.Lesson(2);
+            //student2.Lesson(4);
+            //student2.Lesson(4);
+            //student2.Lesson(2);
+            //student2.Lesson(0);
+            //student2.Lesson(0);
+            //student2.Lesson(0);
+            //student2.Lesson(0);
+
+            //White_3.Student[] students = new White_3.Student[2];
+            //students[0] = student1;
+            //students[1] = student2;
+
+            //Console.WriteLine("До:");
+            //Console.WriteLine();
+            //foreach (var student in students)
+            //{
+            //    student.Print();
+            //}
+            //White_3.Student.SortBySkipped(students);
+
+            //Console.WriteLine();
+            //Console.WriteLine("После:");
+            //Console.WriteLine();
+
+            //foreach (var student in students)
+            //{
+            //    student.Print();
+            //}
+
+            //#4
+
+            //White_4.Participant participant1 = new White_4.Participant("Никита", "Батов");
+            //participant1.PlayMatch(0.0);
+            //participant1.PlayMatch(1.0);
+            //participant1.PlayMatch(0.5);
+            //participant1.PlayMatch(0.5);
+            //participant1.PlayMatch(0.5);
+            //participant1.PlayMatch(1.0);
+            //participant1.PlayMatch(0.5);
+            //participant1.PlayMatch(1.0);
+            //participant1.PlayMatch(0.5);
+            //participant1.PlayMatch(1.0);
+
+            //White_4.Participant participant2 = new White_4.Participant("Артём", "Иванов");
+            //participant2.PlayMatch(0.0);
+            //participant2.PlayMatch(0.0);
+            //participant2.PlayMatch(1.0);
+            //participant2.PlayMatch(0.0);
+            //participant2.PlayMatch(0.5);
+            //participant2.PlayMatch(0.5);
+            //participant2.PlayMatch(0.5);
+            //participant2.PlayMatch(1.0);
+            //participant2.PlayMatch(0.5);
+            //participant2.PlayMatch(0.5);
+
+            //White_4.Participant participant3 = new White_4.Participant("Артём", "Иванов");
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(1.0);
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(1.0);
+            //participant3.PlayMatch(0.0);
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(0.5);
+            //participant3.PlayMatch(0.5);
+
+            //White_4.Participant[] participants = {participant2, participant1, participant3};
+            //foreach (var participant in participants)
+            //{
+            //    participant.Print();
+            //}
+
+            //Console.WriteLine("После сортировки");
+            //Console.WriteLine();
+
+            //White_4.Participant.Sort(participants);
+
+            //foreach (var participant in participants)
+            //{
+            //    participant.Print();
+            //}
+
+        }
+    }
+}
---
+++ White_4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using static LAB_6.White_3;
+
+namespace LAB_6
+{
+    internal class White_4
+    {
+        public struct Participant
+        {
+            private string _name;
+            private string _surname;
+            private double[] _scores;
+            private int _count;
+
+            public string Surname => _surname;
+            public string Name => _name;
+            public double[] Scores => _scores;
+            public double TotalScores => _scores.Sum();
+
+            public Participant(string name, string surname)
+            {
+                _name = name;
+                _surname = surname;
+                _scores = new double[0];
+                _count = 0;
+            }
+            public void PlayMatch(double result)
+            {
+                double[] temp = new double[_scores.Length+1];
+                for(int i = 0;i < _scores.Length; i++)
+                {
+                    temp[i] = _scores[i];
+                }
+                temp[temp.Length-1] = result;
+                _scores = temp;
+            }
+            public static void Sort(Participant[] array)
+            {
+                int length = array.Length;
+                for (int i = 0; i < length - 1; i++)
+                {
+                    for (int j = 0; j < length - i - 1; j++)
+                    {
+                        if (array[j].TotalScores < array[j + 1].TotalScores)
+                        {
+                            Participant temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+
+            }
+            public void Print()
+            {
+                Console.WriteLine($"{Surname} {Name}, кол-во баллов {TotalScores}");
+            }
+        }
+    }
+}
