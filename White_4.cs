70c324baa6cab2fba2f6ca1f32aa7d00176d94d5
VanyaNNi <93041998+VanyaNNi@users.noreply.github.com>
2025-03-25 14:57:35 +0300

    Update White_4.cs

--- White_4.cs
+++ White_4.cs
@@ -1,14 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Schema;
-using static LAB_6.White_3;
 
-namespace LAB_6
+namespace Lab_6
 {
-    internal class White_4
+    public class White_4
     {
         public struct Participant
         {
@@ -20,7 +18,7 @@ namespace LAB_6
             public string Surname => _surname;
             public string Name => _name;
             public double[] Scores => _scores;
-            public double TotalScores => _scores.Sum();
+            public double TotalScore => _scores.Sum();
 
             public Participant(string name, string surname)
             {
@@ -31,12 +29,12 @@ namespace LAB_6
             }
             public void PlayMatch(double result)
             {
-                double[] temp = new double[_scores.Length+1];
-                for(int i = 0;i < _scores.Length; i++)
+                double[] temp = new double[_scores.Length + 1];
+                for (int i = 0; i < _scores.Length; i++)
                 {
                     temp[i] = _scores[i];
                 }
-                temp[temp.Length-1] = result;
+                temp[temp.Length - 1] = result;
                 _scores = temp;
             }
             public static void Sort(Participant[] array)
@@ -46,7 +44,7 @@ namespace LAB_6
                 {
                     for (int j = 0; j < length - i - 1; j++)
                     {
-                        if (array[j].TotalScores < array[j + 1].TotalScores)
+                        if (array[j].TotalScore < array[j + 1].TotalScore)
                         {
                             Participant temp = array[j];
                             array[j] = array[j + 1];
@@ -58,7 +56,7 @@ namespace LAB_6
             }
             public void Print()
             {
-                Console.WriteLine($"{Surname} {Name}, кол-во баллов {TotalScores}");
+                Console.WriteLine($"{Surname} {Name}, кол-во баллов {TotalScore}");
             }
         }
     }
