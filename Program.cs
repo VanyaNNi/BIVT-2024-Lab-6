b697bf60fa3f0212751dffa6995d8f4e52bf8638
VanyaNNi <93041998+VanyaNNi@users.noreply.github.com>
2025-03-25 15:01:09 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LAB_6
+namespace Lab_6
 {
-    internal class Program
+    public class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             //#1
 
